d8e9dc882104a80ec0a200230932c16209e89245
ffigueroa-pixel <figueroacoc@outlook.es>
2023-03-25 21:01:49 -0600

    controller
    controller

--- Assets/Scripts/Player_Character.cs
+++ Assets/Scripts/Player_Character.cs
@@ -5,16 +5,23 @@ using UnityEngine;
 
 public class Player_Character : MonoBehaviour
 {
-    public float speed;
+    public float maxSpeed;
     public float rotationSpeed;
     private CharacterController controller;
     public float JumpSpeed;
     private float ySpeed;
-
+    private Animator animator;
+    public Transform TransformCamera;
+    private float originalSetOffset;
+    private bool isJumping;
+    private bool isGrounded;
+    private float jumpHorizontalSped;
     // Start is called before the first frame update
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+        originalSetOffset = controller.stepOffset;
     }
 
     // Update is called once per frame
@@ -28,25 +35,77 @@ public class Player_Character : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
         Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
-        float magnitud = Mathf.Clamp01(movementDirection.magnitude)*speed;
+        float v = Mathf.Clamp01(movementDirection.magnitude);
+
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+        {
+            v /= 0.5f;
+        }
+
+        animator.SetFloat("InputMagnitude", v, 0.5f, Time.deltaTime);
+        float speed = v * maxSpeed;
+        movementDirection = Quaternion.AngleAxis(TransformCamera.rotation.eulerAngles.y, Vector3.up) * movementDirection;
         movementDirection.Normalize();
         ySpeed += Physics.gravity.y * Time.deltaTime;
 
-        if (Input.GetButtonDown("Jump") && (controller.isGrounded))
+        if (controller.isGrounded)
         {
-            ySpeed = 0;
-            ySpeed = JumpSpeed;
+            controller.stepOffset = originalSetOffset;
+            ySpeed = -0.5f;
+            animator.SetBool("IsGrounded", true);
+            isGrounded = true;
+            animator.SetBool("IsJumping", false);
+            isJumping = false;
+            animator.SetBool("IsFalling", false);
+            
+            if (Input.GetButtonDown("Jump"))
+            {
+                ySpeed = JumpSpeed;
+                animator.SetBool("IsJumping", true);
+                isJumping = true;
+            }
+            
         }
-        Vector3 velocity = movementDirection * magnitud;
+        else
+        {
+            animator.SetBool("IsGrounded", false);
+            isGrounded = false;
+            if((isJumping && ySpeed < 0) || ySpeed< -2 )
+            {
+                animator.SetBool("IsFalling", true);
+            }
+
+        }
+        Vector3 velocity = movementDirection * speed;
         velocity.y = ySpeed;
         //transform.Translate(movementDirection*speed*Time.deltaTime,Space.World);
 
         controller.Move(velocity * Time.deltaTime);
 
-        if(movementDirection != Vector3.zero)
+        if (movementDirection != Vector3.zero)
         {
             Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
-            transform.rotation = Quaternion.RotateTowards(transform.rotation,toRotation, rotationSpeed* Time.deltaTime);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+        }
+
+        if(isGrounded == false)
+        {
+            Vector3 velocidad = movementDirection * jumpHorizontalSped;
+            velocidad.y = ySpeed;
+            controller.Move(velocidad * Time.deltaTime);
         }
     }
+
+    private void OnApplicationFocus(bool focus)
+    {
+        if (focus)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+
 }
